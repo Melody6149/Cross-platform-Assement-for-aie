ff647be5b916fc59daea8b84bde386044230609b
Melody <reid.eastin@gmail.com>
2020-05-07 23:14:12 -0500

    I added a main menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+
+    public void LoadScene(int sceneBuildIndex)
+    {
+        SceneManager.LoadScene(sceneBuildIndex);
+    }
+}
